c5456f7d9026086d584196986d325ba1455dfba2
agarbar439 <ggarciabarreraantonio@gmail.com>
2024-09-23 17:26:37 +0200

    actividad 3- calcular factorial en funcion

--- Tema1/actividad2.php
+++ Tema1/actividad2.php
@@ -3,7 +3,7 @@
 /*Escribe una función para calcular potencias. Recibirá como argumento la base y el exponente, 
 que es opcional y tiene valor por defecto 2 (elevar al cuadrado).*/
 
-// Fuuncion para calcular una potencia
+// Funcion para calcular una potencia
 function calcularPotencia($base, $exponente = 2) // Exponente opcional o 2 por defecto
 {
 
---
+++ Tema1/actividad3.php
@@ -0,0 +1,38 @@
+<?php
+// Antonio Garcia Barrera
+// Escribe una función para calcular el factorial de un número, que recibirá como argumento. Devolverá el factorial o -1 si el argumento no es válido.
+
+//Funcion para calcular el factorial con un valor por argumento
+
+function calculador($operacion, $num)
+{
+    $resultado = $operacion($num); // Llamar a la operación con el número
+    return $resultado;
+}
+
+function factorial($num)
+{
+    if ($num <= 0 || is_string($num)) { // Si el numero es menor o igual que 0 o es string
+        return -1;
+    } else {
+        // Calculo del factorial
+        $factorial = 1; // Valor por defecto
+        for ($i = 1; $i <= $num; $i++) {
+            // En cada iteración se multiplica el valor acumulado de factorial por el valor actual de $i
+            $factorial *= $i;
+        }
+        return $factorial; // Devolver el factorial
+    }
+}
+
+// Ejemplo para calcular el factorial
+$a = 5;
+$r1 = calculador("factorial", $a);
+echo "Factorial de $a: " . $r1 . "\n";
+
+// Ejemplo con un valor invalido
+$b = -3;
+$r2 = calculador("factorial", $b);
+echo "Factorial de $b: " . $r2 . "\n";
+
+?>
