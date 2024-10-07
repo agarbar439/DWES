aa13de027128cfc1c193c06959687123e28efd3c
agarbar439 <ggarciabarreraantonio@gmail.com>
2024-10-07 17:18:32 +0200

    a

--- Tema3/actividad3_4_antonio_garcia_barrera.php
+++ Tema3/actividad3_4_antonio_garcia_barrera.php
@@ -10,20 +10,19 @@ try {
     // Variable con al conexion
     $bd = new PDO($cadena_conexion, $usuario, $clave);
     // Mensaje conexion exitosa
-    echo "Conexión exitosa". "<br>";
+    echo "Conexión exitosa" . "<br>";
     // Consulta a la base de datos
     $sql = 'SELECT nombre, apellido, rol, ciudad FROM usuarios WHERE id = 2';
     // Almacenar en la variable el resultado de ejecutar la consulta
     $resultado = $bd->query($sql);
-    
-    // Mostrar por pantalla los datos
-    foreach($resultado as $row){
-    print "Nombre: ". $row['nombre']. "<br>";
-    print "Apellido: ". $row['apellido']. "<br>";
-    print "Rol: ". $row['rol']. "<br>";
-    print "Ciudad: ". $row['ciudad']. "<br>";
+
+    // Mostrar spor pantalla los datos
+    foreach ($resultado as $row) {
+        print "Nombre: " . $row['nombre'] . "<br>";
+        print "Apellido: " . $row['apellido'] . "<br>";
+        print "Rol: " . $row['rol'] . "<br>";
+        print "Ciudad: " . $row['ciudad'] . "<br>";
     }
 } catch (PDOException $e) {
     echo 'Error base de datos: ' . $e->getMessage();
 }
-?>
